e60cdefc8a86b68b0ad85408db107370ed590540
MookerVart076 <asper7188@gmail.com>
2021-09-14 21:51:32 +0700

    tugas (contoh1)

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Aldo Wijaya Kusuma
+        saya tinggal di Jakarta makanan favorit
+        saya adalah Nasi";
+    }
+}
